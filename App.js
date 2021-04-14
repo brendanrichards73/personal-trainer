c71200658f958f346f44bf416073b8d24fe04aec
brendan richards <brendanrichards@economist.com>
2021-04-14 15:00:42 +0100

    refactored screens out of app.js

--- App.js
+++ App.js
@@ -1,40 +1,11 @@
 import * as React from 'react';
-import {Text, View} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import Ionicons from 'react-native-vector-icons/Ionicons';
-
-function HomeScreen() {
-  return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text>Home!</Text>
-    </View>
-  );
-}
-
-function FitnessPlans() {
-  return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text>Fitness Plans</Text>
-    </View>
-  );
-}
-
-function ProfileScreen() {
-  return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text>Profile</Text>
-    </View>
-  );
-}
-
-function SettingsScreen() {
-  return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text>Settings</Text>
-    </View>
-  );
-}
+import {Home} from './personal-trainer/src/home/home';
+import {FitnessPlans} from './personal-trainer/src/fitness-plans/fitness-plans';
+import {Settings} from './personal-trainer/src/settings/settings';
+import {Profile} from './personal-trainer/src/profile/profile';
 
 const Tab = createBottomTabNavigator();
 
@@ -59,7 +30,6 @@ export default function App() {
               iconName = focused ? 'ios-list' : 'ios-list';
             }
 
-
             return <Ionicons name={iconName} size={size} color={color} />;
           },
         })}
@@ -67,10 +37,10 @@ export default function App() {
           activeTintColor: 'tomato',
           inactiveTintColor: 'gray',
         }}>
-        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Home" component={Home} />
         <Tab.Screen name="Fitness Plans" component={FitnessPlans} />
-        <Tab.Screen name="Profile" component={ProfileScreen} />
-        <Tab.Screen name="Settings" component={SettingsScreen} />
+        <Tab.Screen name="Profile" component={Profile} />
+        <Tab.Screen name="Settings" component={Settings} />
       </Tab.Navigator>
     </NavigationContainer>
   );
