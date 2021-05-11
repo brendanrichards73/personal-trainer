09b23f5d4dd692d2836c05c68d3c2c09f003ee33
brendan richards <brendanrichards@economist.com>
2021-05-11 15:57:25 +0100

    added api call for workouts

--- src/api/api.js
+++ src/api/api.js
@@ -5,7 +5,14 @@ export const getUser = async (userId) => {
   return response.data.data;
 };
 
-export const getWorkOuts = async () => {
+export const getExerciseTypes = async () => {
   const response = await axios.get('https://wger.de/api/v2/exercisecategory/');
+  return response.data.results;
+};
+
+export const getWorkouts = async (number) => {
+  const response = await axios.get(
+    `https://wger.de/api/v2/exercise/?language=2&category=${number}`,
+  );
   return response.data.results;
 };
--- src/fitness-plans/fitness-plans.js
+++ src/fitness-plans/fitness-plans.js
@@ -1,7 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import {View, StyleSheet} from 'react-native';
 import {FitnessItem} from './fitness-item';
-import {getWorkOuts} from '../api/api';
+import {getExerciseTypes} from '../api/api';
 
 const styles = StyleSheet.create({
   container: {
@@ -14,14 +14,14 @@ const styles = StyleSheet.create({
 export const FitnessPlans = () => {
   const [workouts, setWorkouts] = useState([]);
   useEffect(() => {
-    getWorkOuts().then((response) => {
+    getExerciseTypes().then((response) => {
       setWorkouts(response);
     });
   }, []);
   return (
     <View style={styles.container}>
       {workouts.map(({id, name}) => (
-        <FitnessItem key={id} workOutName={name} />
+        <FitnessItem key={id} workOutName={name} id={id} />
       ))}
     </View>
   );
