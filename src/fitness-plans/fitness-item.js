beef8321e411620683456ae7f5912c826f4261b1
brendan richards <brendanrichards@economist.com>
2021-05-18 14:55:50 +0100

    created exercises

--- src/fitness-plans/fitness-item.js
+++ src/fitness-plans/fitness-item.js
@@ -1,6 +1,24 @@
-import React from 'react';
-import {Text} from 'react-native';
+import React, {useState} from 'react';
+import {Text, TouchableOpacity} from 'react-native';
+import {getWorkouts} from '../api/api';
 
 export const FitnessItem = ({workOutName, id}) => {
-  return <Text>{workOutName}</Text>;
+  const [exercises, setExercises] = useState([]);
+  const clickHandler = () => {
+    getWorkouts(id).then((response) => {
+      setExercises(response);
+      console.log('I am workout', response);
+    });
+    console.log('I have been clicked with id', id);
+  };
+  return (
+    <>
+      <TouchableOpacity onPress={clickHandler}>
+        <Text>{workOutName}</Text>
+      </TouchableOpacity>
+      {exercises.map(({name}) => (
+        <Text>{name}</Text>
+      ))}
+    </>
+  );
 };
