4fa8b21f3a5977954c8c58e94a256efb2c70fb34
brendan richards <brendanrichards@economist.com>
2021-09-10 16:40:31 +0100

    added home screen colour

--- src/pages/home.js
+++ src/pages/home.js
@@ -4,15 +4,23 @@ import {Text, View, StyleSheet} from 'react-native';
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#B4F5F6',
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  text: {
+    justifyContent: 'center',
+    color: '#62ABAC',
+    alignItems: 'center',
+    fontFamily: 'Milote',
+    fontSize: 35,
   },
 });
 
 export const Home = () => {
   return (
     <View style={styles.container}>
-      <Text>Home!</Text>
+      <Text style={styles.text}>Luke Morris Fitness</Text>
     </View>
   );
 };
