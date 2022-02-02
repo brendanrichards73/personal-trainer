aa7358ed75b79c0bea0fbf1dc773500a247468c3
brendan richards <brendanrichards@economist.com>
2022-02-02 15:19:44 +0000

    added register ui to profile screen

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -1,6 +1,6 @@
-import React, {useState, useEffect} from 'react';
-import {Text, View, StyleSheet} from 'react-native';
-import {getUser} from '../api/api';
+import React, {useState} from 'react';
+import {Text, View, StyleSheet, Button} from 'react-native';
+import {TextInput} from 'react-native-gesture-handler';
 
 // create profile input to store in cloud.
 
@@ -10,19 +10,40 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
+  loginContainer: {
+    width: 200,
+    height: 100,
+    borderWidth: 1,
+    borderColor: 'grey',
+    marginVertical: 20,
+  },
 });
 
 export const Profile = () => {
-  const [name, setName] = useState('');
-  useEffect(() => {
-    getUser().then((response) => {
-      setName(response.first_name);
-    });
-  }, []);
+  const [text, setText] = useState('');
+  const [password, setPassword] = useState('');
+  const register = () => {
+    console.log('Entered text', text);
+    console.log('Entered password', password);
+  };
 
   return (
     <View style={styles.container}>
-      <Text>{name}</Text>
+      <Text fontFamily="Cochin">Register</Text>
+      <View style={styles.loginContainer}>
+        <TextInput
+          placeholder="Name"
+          fontFamily="Cochin"
+          onChangeText={(newText) => setText(newText)}
+        />
+        <TextInput
+          placeholder="Password"
+          fontFamily="Cochin"
+          secureTextEntry
+          onChangeText={(newPassword) => setPassword(newPassword)}
+        />
+      </View>
+      <Button title="Enter" onPress={register} />
     </View>
   );
 };
